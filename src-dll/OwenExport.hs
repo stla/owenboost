69960396ce0d953314f43416452b9cdbc98bdaaf
stla <laurent_step@yahoo.fr>
2017-12-14 07:33:28 +0100

    powenExport

--- src-dll/OwenExport.hs
+++ src-dll/OwenExport.hs
@@ -16,3 +16,16 @@ owenQexport nu t delta r n result = do
   delta <- peekArray (fromIntegral n) delta
   r <- peekArray (fromIntegral n) r
   (>>=) (owenQ128 nu t delta r) (pokeArray result)
+
+foreign export ccall powenExport :: Ptr CInt -> Ptr CDouble -> Ptr CDouble ->
+                  Ptr CDouble -> Ptr CDouble -> Ptr CInt -> Ptr CDouble -> IO ()
+powenExport :: Ptr CInt -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble ->
+                                Ptr CDouble -> Ptr CInt -> Ptr CDouble -> IO ()
+powenExport nu t1 t2 delta1 delta2 n result = do
+  nu <- peek nu
+  t1 <- peek t1
+  t2 <- peek t2
+  n <- peek n
+  delta1 <- peekArray (fromIntegral n) delta2
+  delta2 <- peekArray (fromIntegral n) delta2
+  (>>=) (powen nu t1 t2 delta1 delta2) (pokeArray result)
